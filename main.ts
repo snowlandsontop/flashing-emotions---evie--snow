78d90e6efb35d8ca8491ffe6e0b65dbd9d1e5946
snowlandsontop <152302239+snowlandsontop@users.noreply.github.com>
2023-12-11 09:04:04 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 input.onButtonPressed(Button.A, function () {
     for (let index = 0; index < 4; index++) {
-        basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.Happy)
         basic.pause(200)
         basic.clearScreen()
         basic.pause(200)
@@ -8,7 +8,7 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.B, function () {
     for (let index = 0; index < 4; index++) {
-        basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.Sad)
         basic.pause(200)
         basic.clearScreen()
         basic.pause(200)
